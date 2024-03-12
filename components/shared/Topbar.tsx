b899f17da81318e070311d7796b8674745043b92
Koome-eric <73523878+Koome-eric@users.noreply.github.com>
2024-03-12 10:56:20 +0300

    Update Topbar.tsx

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -8,7 +8,7 @@ function Topbar() {
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
         <Image src='/logo.svg' alt='logo' width={40} height={40} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Pivotly</p>
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>GIVESUITE</p>
       </Link>
 
       <div className='flex items-center gap-4'>
