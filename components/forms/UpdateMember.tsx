e154183e3243fa1453125b3de762abd7d87d4626
Koome-eric <73523878+Koome-eric@users.noreply.github.com>
2024-03-25 14:14:31 +0300

    Update UpdateMember.tsx

--- components/forms/UpdateMember.tsx
+++ components/forms/UpdateMember.tsx
@@ -51,7 +51,7 @@ function UpdateMember({ memberId, initialData }: Props) {
             name={fieldName as keyof z.infer<typeof MemberValidation>} // Cast fieldName to the correct type
             render={({ field }) => (
               <FormItem className='flex w-full flex-col gap-3'>
-                <FormLabel className='text-base-semibold text-light-2'>
+                <FormLabel className='text-base-semibold text-dark-2'>
                   {fieldName.charAt(0).toUpperCase() + fieldName.slice(1).replace(/([A-Z])/g, ' $1').trim()}
                 </FormLabel>
                 <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
